9075168e4bafd7dd0808ecf3fa5248c3d0237e56
0art0 <18333981+0art0@users.noreply.github.com>
2025-04-23 12:29:40 +0100

    Parser for primitive roots

---
+++ MetaCompute/Parsers.lean
@@ -0,0 +1,42 @@
+import Std.Internal.Parsec.String
+
+open Lean Std Internal Parsec String
+
+partial def parseFactors : String.Parser (List (Nat × Nat)) := do
+  if ← isEof then
+    return []
+  ws
+  skipChar '['
+  ws
+  let p ← digits
+  ws
+  let e ← digits
+  ws
+  skipChar ']'
+  ws
+  let tail ← parseFactors
+  return (p, e) :: tail
+
+def getFactors (s : String) : IO <| List (Nat × Nat) := do
+  IO.ofExcept <| parseFactors.run s
+
+def parsePrimitiveRootOf (base : Nat) : String.Parser Nat := do
+  ws
+  skipString "Mod"
+  skipChar '('
+  ws
+  let root ← digits
+  ws
+  skipChar ','
+  ws
+  let p ← digits
+  unless p == base do
+    fail s!"Expected base {base}, but got {p}"
+  ws
+  skipChar ')'
+  ws
+  eof
+  return root
+
+def getPrimitiveRootOf (base : Nat) (s : String) : IO <| Nat := do
+  IO.ofExcept <| (parsePrimitiveRootOf base).run s
