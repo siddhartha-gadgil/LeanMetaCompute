e9b5816833329fb9d56d70617dae78151584d865
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2025-04-26 10:52:06 +0000

    pratt certification

--- MetaCompute/Pari.lean
+++ MetaCompute/Pari.lean
@@ -53,20 +53,87 @@ end pari
 #eval znPrimRoot 85083351022467190124442353598696803287939269665617 -- 5
 
 def listProduct (l : List (Nat × Nat)) : Nat :=
-  l.foldl (init := 1) (fun acc (x : Nat × Nat) => acc * x.1 ^ x.2)
+  match l with
+  | [] => 1
+  | (x, y) :: xs =>
+    let prod := listProduct xs
+    let x_pow_y := x ^ (y + 1)
+    x_pow_y * prod
+
+theorem listProduct_nil : listProduct [] = 1 := by
+  simp [listProduct]
+
+theorem listProduct_cons (x : Nat × Nat) (xs : List (Nat × Nat)) :
+  listProduct (x :: xs) = x.1 ^ (x.2 + 1) * listProduct xs := by
+  simp [listProduct]
 
 structure PrattCertificate (p : Nat) where
+  p_ne_one : p ≠ 1
   a : Nat
-  a_pow_pminus_1 : powerMod a (p - 1) = 1
+  a_pow_pminus_1 : powerMod a (p - 1) p = 1
   factors : List (Nat × Nat)
   factors_correct : listProduct factors = p - 1
-  a_pow_p_by_d_minus_1 : ∀ pair ∈ factors, powerMod a (p / pair.1) ≠  1
+  a_pow_p_by_d_minus_1 : ∀ pair ∈ factors, powerMod a ((p - 1) / pair.1) p ≠  1
   factors_prime : ∀ pair ∈ factors, Nat.Prime pair.1
+
+theorem List.prime_div_is_factor (l: List (Nat × Nat))
+    (prod: listProduct l = n) (primes : ∀ pair ∈ l, Nat.Prime pair.1) :
+    ∀ (q : ℕ), q ∣ n → Nat.Prime q →
+      ∃ k : Nat, (q, k) ∈ l := by
+  match l with
+  | [] =>
+    simp [listProduct] at prod
+    intro q q_div_n prime_q
+    simp [← prod] at q_div_n
+    simp [q_div_n] at prime_q
+    apply False.elim
+    apply Nat.prime_one_false
+    assumption
+  | (q', m) :: xs =>
+    intro q q_div_n prime_q
+    simp [listProduct_cons] at prod
+    simp [List.mem_cons]
+    rw [← prod] at q_div_n
+    rw [Nat.Prime.dvd_mul prime_q] at q_div_n
+    if c: q = q' then
+      rw [← c] at q_div_n
+      simp [c]
+    else
+      cases c':q_div_n
+      case inl d =>
+        have d' := Nat.Prime.dvd_of_dvd_pow prime_q d
+        have h' := primes (q', m) (by simp [List.mem_cons])
+        simp at h'
+        rw [Nat.prime_dvd_prime_iff_eq prime_q h'] at d'
+        simp [c, d']
+      case inr d =>
+        let n' := listProduct xs
+        let prod' : listProduct xs = n' := rfl
+        let primes' : ∀ pair ∈ xs, Nat.Prime pair.1 := by
+          intro pair h
+          apply primes
+          apply List.mem_cons_of_mem
+          assumption
+        let ih := List.prime_div_is_factor xs prod' primes' q d prime_q
+        let ⟨k, factor⟩ := ih
+        use k
+        right
+        assumption
+
+#check Nat.Prime.dvd_of_dvd_pow
+
+#check Nat.Prime.dvd_mul
+#check Nat.prime_one_false
+#check Nat.prime_dvd_prime_iff_eq
+
+#check List.foldl_cons
 
 theorem PrattCertificate.prime_dvd_is_factor {p : Nat} (cert : PrattCertificate p) :
   ∀ (q : ℕ), q ∣ p - 1 → Nat.Prime q →
     ∃ k : Nat, (q, k) ∈ cert.factors := by
-  sorry
+  intro q q_div_pminus1 prime_q
+  apply
+    cert.factors.prime_div_is_factor cert.factors_correct cert.factors_prime q q_div_pminus1 prime_q
 
 /-
 lucas_primality (p : ℕ) (a : ZMod p) (ha : a ^ (p - 1) = 1)
@@ -76,9 +143,45 @@ lucas_primality (p : ℕ) (a : ZMod p) (ha : a ^ (p - 1) = 1)
 
 theorem pratt_certification (p : Nat) (cert : PrattCertificate p) : Nat.Prime p := by
   apply lucas_primality p cert.a
-  · sorry
+  · rw [← ZMod.natCast_mod]
+    rw [← Nat.cast_pow]
+    nth_rewrite 2 [← Nat.cast_one]
+    rw [ZMod.natCast_eq_natCast_iff, Nat.ModEq.eq_1]
+    have h := cert.a_pow_pminus_1
+    rw [powerModDef] at h
+    rw [← Nat.pow_mod]
+    rw [h]
+    symm
+    apply Nat.one_mod_eq_one.2
+    exact cert.p_ne_one
   · intro q prime_q q_div_pminus1
     have h := cert.prime_dvd_is_factor q q_div_pminus1 prime_q
-    sorry
+    have ⟨k, factor⟩ := h
+    have h' := cert.a_pow_p_by_d_minus_1 (q, k) factor
+    rw [powerModDef] at h'
+    simp at h'
+    rw [← ZMod.natCast_mod]
+    rw [← Nat.cast_pow]
+    nth_rewrite 2 [← Nat.cast_one]
+    intro contra
+    rw [ZMod.natCast_eq_natCast_iff, Nat.ModEq.eq_1] at contra
+    rw [← Nat.pow_mod] at contra
+    rw [contra] at h'
+    apply h'
+    apply Nat.one_mod_eq_one.2
+    exact cert.p_ne_one
+
+#check Nat.one_mod_eq_one
 
 #check Int.ModEq.eq
+#check Nat.ModEq.eq_1
+
+#loogle (_ % ?p) ^ _ % ?p
+#check ZMod.intCast_eq_intCast_iff
+
+#loogle (_ : ZMod _) ^ (_ : ℕ) = (_ : ZMod _)
+
+#check ZMod.intCast_eq_intCast_iff
+#check Nat.cast_one
+
+#check ZMod.natCast_eq_natCast_iff
