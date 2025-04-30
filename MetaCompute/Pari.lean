855b17eb77a15eff176d52bd8df4b951b9ec5e8c
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2025-04-30 10:18:31 +0000

    partial pratt solution; bug with mvars

--- MetaCompute/Pari.lean
+++ MetaCompute/Pari.lean
@@ -134,6 +134,11 @@ def fromExpPair : Syntax → MetaM (ℕ × ℕ)
 | `(exp_pair|( $a ^ $b)) => return (a.getNat, b.getNat)
 | _ => throwUnsupportedSyntax
 
+def toExpPair (pair : ℕ × ℕ) : MetaM <| TSyntax `exp_pair :=
+  let aStx := Syntax.mkNumLit (toString pair.1)
+  let bStx := Syntax.mkNumLit (toString pair.2)
+  `(exp_pair| ( $aStx ^ $bStx))
+
 elab "pratt_certificate_for_safe%" p:num "using" a:num : term => do
   let pExpr ← elabTerm p (mkConst ``Nat)
   let tgt ← mkAppM ``PrattCertificate #[pExpr]
@@ -291,6 +296,9 @@ example : Nat.Prime 48611 := by
   prime
   · prime
 
+example : Nat.Prime 85083351022467190124442353598696803287939269665617 := by repeat prime
+
+
 def prattReduce (a : ℕ) (factors : List (ℕ × ℕ)) (goal : MVarId)  : MetaM <| List MVarId := goal.withContext do
   let_expr Nat.Prime pE := ←  goal.getType | throwError "target is not of the form `Nat.Prime _`"
   let some p ← getNatValue? (← reduce pE) | throwError "Failed to obtain a natural number from {pE}"
@@ -312,7 +320,7 @@ def prattReduce (a : ℕ) (factors : List (ℕ × ℕ)) (goal : MVarId)  : MetaM
   return goals
 
 
-elab "pratt_step"  a:num "[" fctrs:exp_pair,* "]" : tactic => withMainContext do
+elab "pratt_step"  a:num "[" fctrs:exp_pair,* "]" : tactic  => withMainContext do
   let a := a.getNat
   let factors ← fctrs.getElems.toList.mapM
     fun pair => fromExpPair pair
@@ -325,14 +333,51 @@ example : Nat.Prime 19 := by
   · decide
 
 
-example : Nat.Prime 85083351022467190124442353598696803287939269665617 := by repeat prime
+partial def primeScript (goal : MVarId) :
+  MetaM <| List Syntax.Tactic := goal.withContext do
+  let tgt ← reduce (← goal.getType)
+  let_expr Nat.Prime pE :=   tgt | throwError s!"target {← ppExpr tgt} is not of the form `Nat.Prime _`"
+  let some p ← getNatValue? (← reduce pE) | throwError "Failed to obtain a natural number from {pE}"
+  if p < 100 then
+    unless Nat.Prime p do
+      throwError "{pE} is not a prime"
+    discard <| runTactic goal <| ← `(tactic|norm_num)
+    return [← `(tactic|norm_num)]
+  else
+    let check ← queryPari s!"isprime({p})"
+    unless check = "1" do
+      throwError "{pE} is not a prime (according to Pari)"
+    let a ← znPrimRoot p
+    logInfo m!"Primitive root: {a}"
+    let factors := (← factors (p - 1))
+    logInfo m!"Factors: {factors}"
+    let newGoals ← prattReduce a factors goal
+    let aStx := Syntax.mkNumLit (toString a)
+    let factorStxs ←  factors.mapM (fun (x : Nat × Nat) => toExpPair x)
+    let factorStxs := factorStxs.toArray
+    logInfo m!"factorStxs: {factorStxs}"
+    let head ← `(tactic|pratt_step $aStx [$factorStxs,*])
+    let tail ← newGoals.mapM fun goal => do
+      let stx ← primeScript goal
+      let stx := stx.toArray
+      `(tactic| · $stx*)
+    logInfo m!"head: {head}"
+    return head :: tail
 
-#check Nat.one_mod_eq_one
+syntax (name := pratt_tac) "pratt" : tactic
 
-#check Int.ModEq.eq
-#check Nat.ModEq.eq_1
+@[tactic pratt_tac]
+def prattImpl : Tactic := fun stx => withMainContext do
+  match stx with
+  | `(tactic|pratt) => withMainContext do
+    let scripts ←  primeScript (← getMainGoal)
+    let scripts := scripts.toArray
+    TryThis.addSuggestion stx <| ← `(tactic|· $scripts*)
+    return
+  | _ =>
+    throwUnsupportedSyntax
 
-#check ZMod.intCast_eq_intCast_iff
-#check Nat.cast_one
+#check prattImpl
 
-#check ZMod.natCast_eq_natCast_iff
+example : Nat.Prime 191 := by
+  pratt
